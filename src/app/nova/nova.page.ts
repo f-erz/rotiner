46638e71890c5bcb5d28bfded2546cedd6e8df2d
f-erz <fernando_zaninelli@hotmail.com>
2023-09-09 22:26:20 -0300

    First commit

---
+++ src/app/nova/nova.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nova',
+  templateUrl: './nova.page.html',
+  styleUrls: ['./nova.page.scss'],
+})
+export class NovaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
